1a66629e20b18ec7125b226db6b48cdb8707f977
Julia Veronica <julia.veronicagdu@gmail.com>
2020-04-12 19:10:59 +0700

    error di event listener dan change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let data = [];
 axios.get('http://localhost:3000/stock')
     .then((response) => {
-        const barang = document.getElementById("listBarang")
+        const barang = document.getElementById("listBarang");
         data = response.data;
         console.log(data);
 
@@ -21,8 +21,8 @@ axios.get('http://localhost:3000/stock')
         <td>${quantity}</td>
         <td>${price}</td>
         <td>
-        <button onclick="ganti(${id})"class="btn btn-outline-primary"><i class="fa fa-pencil-square">&nbsp;&nbsp;Change</button></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
-        <button onclick="hapus(${id})" class="btn btn-outline-danger"><i class="fa fa-ban">&nbsp;&nbsp;Delete</button></i>
+        <button onClick="ganti(${id})"class="btn btn-outline-primary"><i class="fa fa-pencil-square">Change</button></i>
+        <button onClick="deleteData(${id})" class="btn btn-outline-danger"><i class="fa fa-ban">Delete</button></i>
         </td>
         </tr>`;
             barang.innerHTML += itemHTML;
@@ -31,7 +31,11 @@ axios.get('http://localhost:3000/stock')
     .catch((pesanError) => {
         console.log(pesanError);
     })
+    const deleteData = id => {
+        axios.delete(`http://localhost:3000/stock/${id}`)
+    }
 document.getElementById('simpan').addEventListener('click', function (event) {
+    event.preventDefault()
 
     const name = document.getElementById('name').value;
     const quantity = document.getElementById('quantity').value;
@@ -48,23 +52,21 @@ document.getElementById('simpan').addEventListener('click', function (event) {
         console.error(err);
     });
 });
-const hapus = id => {
-    axios.delete(`http://localhost:3000/stock/${id}`)
-}
+
 const ganti = id => {
     const stock = data.find(item => {
-        return item === id
+        return item === id;
     })
     if (stock) {
         const id = window.prompt('Id', stock.id);
         const name = window.prompt('Name', stock.name);
         const price = window.prompt('Price', stock.price);
         const quantity = window.prompt('Quantity', stock.quantity);
-        axios.put(`http://localhost:3000/stock/${quantity}`, {
+        axios.put(`http://localhost:3000/stock/${id}`, {
             id,
             name,
             price,
             quantity
         });
-    }
+    } 
 }
